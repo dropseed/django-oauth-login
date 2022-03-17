c0dc02102ecbe4a858b64965a90ee2869d420106
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-03-17 15:21:30 -0500

    Use environ.get in test settings

--- tests/settings.py
+++ tests/settings.py
@@ -69,26 +69,28 @@ LOGOUT_REDIRECT_URL = "/"
 
 AUTH_USER_MODEL = "users.User"
 
+# OAuth providers to use for a real, interactive test
+# (in a real config you'd probably do environ["key"] to raise a KeyError if an env var is forgotten)
 OAUTH_LOGIN_PROVIDERS = {
     "github": {
         "class": "providers.github.GitHubOAuthProvider",
         "kwargs": {
-            "client_id": environ["GITHUB_CLIENT_ID"],
-            "client_secret": environ["GITHUB_CLIENT_SECRET"],
+            "client_id": environ.get("GITHUB_CLIENT_ID"),
+            "client_secret": environ.get("GITHUB_CLIENT_SECRET"),
         },
     },
     "bitbucket": {
         "class": "providers.bitbucket.BitbucketOAuthProvider",
         "kwargs": {
-            "client_id": environ["BITBUCKET_KEY"],
-            "client_secret": environ["BITBUCKET_SECRET"],
+            "client_id": environ.get("BITBUCKET_KEY"),
+            "client_secret": environ.get("BITBUCKET_SECRET"),
         },
     },
     "gitlab": {
         "class": "providers.gitlab.GitLabOAuthProvider",
         "kwargs": {
-            "client_id": environ["GITLAB_APPLICATION_ID"],
-            "client_secret": environ["GITLAB_APPLICATION_SECRET"],
+            "client_id": environ.get("GITLAB_APPLICATION_ID"),
+            "client_secret": environ.get("GITLAB_APPLICATION_SECRET"),
             "scope": "read_user",
         },
     },
